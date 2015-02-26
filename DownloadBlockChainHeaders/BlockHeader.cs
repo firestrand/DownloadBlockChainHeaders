e6e92099009d2e58f5e805a0565ca98f44641b76
Travis Silvers <firestrand@gmail.com>
2015-02-26 14:17:29 -0500

    Initial Commit

---
+++ DownloadBlockChainHeaders/BlockHeader.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DownloadBlockChainHeaders
+{
+    public class BlockHeader
+    {
+        public string hash { get; set; } //"hash":"00000000000000000790ec59fe9f006a95b610120ed6ab6ce0fcd190240a8a21",
+        public uint ver { get; set; } //"ver":2,
+        public string prev_block { get; set; } //"prev_block":"0000000000000000148469b72ed6ac7f53d4eb88298e932d9136a2f8d0958774",
+        public string mrkl_root { get; set; } //"mrkl_root":"17db49aa849bdfca931dffde80f0d707377a4f27cb5a74c535c085b816c29e13",
+        public uint time { get; set; } //"time":1423796705,
+        public uint bits { get; set; } //"bits":404274055,
+        public uint nonce { get; set; } //"nonce":2482359827,
+    }
+}
---
+++ DownloadBlockChainHeaders/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace DownloadBlockChainHeaders
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var webClient = new WebClient();
+            var uriRoot = "https://blockexplorer.com/rawblock/";
+            var hash = "00000000000000000790ec59fe9f006a95b610120ed6ab6ce0fcd190240a8a21";
+            var first = webClient.DownloadString(uriRoot + hash);//Last as of 2015/02/2015
+
+            var headerObject = JsonConvert.DeserializeObject<BlockHeader>(first);
+            Console.WriteLine(headerObject.hash);
+            for (int i = 0; i < 10; i++)
+            {
+                headerObject = JsonConvert.DeserializeObject<BlockHeader>(webClient.DownloadString(uriRoot + headerObject.prev_block));
+                Console.WriteLine(headerObject.hash);
+            }
+            Console.ReadKey();
+        }
+    }
+}
